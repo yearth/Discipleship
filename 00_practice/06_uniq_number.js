598713461c07bf1bfa8d3fdb774c78c500393fcc
yearthmain <yearthmain@gmail.com>
2022-04-06 23:17:32 +0800

    Prictice: 06 找到数组中唯一不重复的数字

--- 00_practice/06_uniq_number.js
+++ 00_practice/06_uniq_number.js
@@ -11,7 +11,7 @@
  */
 
 /**
- * @description 思路一：
+ * @description 思路一：借助排序，最基础的思路
  */
 const findUniqNumberBySort = arr => {
   const sortedArr = arr.sort((a, b) => a - b);
@@ -38,4 +38,43 @@ const findUniqNumberBySort = arr => {
     }
   }
 };
-console.log(findUniqNumberBySort([1, 2, 3, 4, 3, 2, 1]));
+// console.log(findUniqNumberBySort([1, 2, 3, 4, 3, 2, 1]));
+
+/**
+ * @description 思路二：借助缓存，缺点是增加空间复杂度
+ */
+const findUniqNumberByCache = arr => {
+  const cache = new Map();
+
+  for (const i of arr) {
+    cache.has(i) ? cache.set(i, cache.get(i) + 1) : cache.set(i, 0);
+  }
+
+  for (const [k, v] of cache) {
+    if (v === 0) {
+      return k;
+    }
+  }
+};
+// console.log(findUniqNumberByCache([1, 2, 3, 4, 3, 2, 1]));
+
+/**
+ * @description 思路三：与思路一类似，只不过借助了 api 来实现
+ */
+const findUniqNumberByAPI = arr => {
+  return arr.find(n => arr.indexOf(n) === arr.lastIndexOf(n));
+};
+// console.log(findUniqNumberByAPI([1, 2, 3, 4, 3, 2, 1]));
+
+/**
+ * @description 思路四：位运算，借助异或
+ *
+ * 任何数与自身异或结果为 0，比如 3 ^ 3 = 0
+ * 0 与任何数异或结果为任何数本身，比如 0 ^ 3 = 3
+ *
+ * 对于本用例，数组异或之后结果为 1 ^ 1 ^ 2 ^ 2 ^ 3 ^ 3 ^ 4 = 4
+ */
+const findUniqNumberByByte = arr => {
+  return arr.reduce((t, c) => t ^ c, 0);
+};
+console.log(findUniqNumberByByte([1, 2, 3, 4, 3, 2, 1]));
